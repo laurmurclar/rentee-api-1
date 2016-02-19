04e7b265178a0ea8a5c60174fb41d056ae004a81
Brian McNestry <brianmcnestry@gmail.com>
2016-02-19 16:58:49 +0000

    added validation to landlord user

--- app/models/landlord.rb
+++ app/models/landlord.rb
@@ -1,2 +1,12 @@
 class Landlord < ActiveRecord::Base
+	before_save {email.downcase!}
+	validates :f_name, presence: true, length: {maximum: 30}
+	validates :l_name, presence: true, length: {maximum: 30}
+	validates :phone, presence: true, length: {minimum: 9, maximum:20}
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: {minimum: 10, maximum: 255},
+						format: { with: VALID_EMAIL_REGEX }, 
+						uniqueness: { case_sensitive: false}
+	has_secure_password
+	validates :password, presence: true, length: { minimum: 6 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160219162934) do
+ActiveRecord::Schema.define(version: 20160219164737) do
 
   create_table "amenities", force: :cascade do |t|
     t.string   "name"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 20160219162934) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
+
+  add_index "landlords", ["email"], name: "index_landlords_on_email", unique: true
 
   create_table "properties", force: :cascade do |t|
     t.string   "building"

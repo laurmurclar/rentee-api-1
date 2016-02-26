b60ba7fa43c6a8bf69e5510006a7735cc22344d5
Laura Murphy-Clarkin <laurmurclar@gmail.com>
2016-02-26 11:23:09 +0000

    remove logger from sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	logger.info("params #{params}")
   	@tenant = Tenant.find_by(email: params[:email].downcase)
-  	logger.info("db password #{@tenant.password}")
-  	logger.info("post password #{params[:password]}")
   	if @tenant && @tenant.authenticate(params[:password])
   		# login_tenant @tenant
   		render json: @tenant
